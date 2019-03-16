0b1c41f1aedfefcebca76813b9d22e5374e5c958
dcrisanto <dorelly.crisanto@gmail.com>
2019-03-16 05:40:05 -0500

    Configuración de Firebase para la app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Usaré la misma base de datos para desarrollo  producción
+  firebaseConfig: {
+    apiKey: "AIzaSyAB7icNPz-tO88wVkgcCeNmlz9H1xd8OTU",
+    authDomain: "login-e3b98.firebaseapp.com",
+    databaseURL: "https://login-e3b98.firebaseio.com",
+    projectId: "login-e3b98",
+    storageBucket: "login-e3b98.appspot.com",
+    messagingSenderId: "857338189328"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Conexión de la plataforma con Firebase
+  firebaseConfig: {
+    apiKey: "AIzaSyAB7icNPz-tO88wVkgcCeNmlz9H1xd8OTU",
+    authDomain: "login-e3b98.firebaseapp.com",
+    databaseURL: "https://login-e3b98.firebaseio.com",
+    projectId: "login-e3b98",
+    storageBucket: "login-e3b98.appspot.com",
+    messagingSenderId: "857338189328"
+  }
 };
 
 /*
