b853851b6cb74bdff3db18c65cca10a304bcb782
dcrisanto <dorelly.crisanto@gmail.com>
2019-03-16 02:34:51 -0500

    Añadi navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+
+const Routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'privado', component: PrivadoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(Routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
